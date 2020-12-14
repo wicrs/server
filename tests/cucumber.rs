c7d9fd1d02f7086b1d199e63659a8345e38844bf
willemml <willem@leit.so>
2020-12-13 22:12:58 -0800

    working login api test with cucumber

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,18 +1,27 @@
 use async_trait::async_trait;
-use std::convert::Infallible;
+use reqwest::Response;
+use std::{convert::Infallible, panic::AssertUnwindSafe};
+use std::sync::Arc;
 
-pub struct APITestWorld {}
+#[derive(Clone)]
+pub struct APITestWorld {
+    response: Arc<AssertUnwindSafe<Option<Response>>>
+}
 
 #[async_trait(?Send)]
 impl cucumber::World for APITestWorld {
     type Error = Infallible;
 
     async fn new() -> Result<Self, Infallible> {
-        Ok(Self {})
+        Ok(Self {
+            response: Arc::new(AssertUnwindSafe(None))
+        })
     }
 }
 
 mod client_steps {
+    use std::{panic::AssertUnwindSafe, sync::Arc};
+
     use cucumber::{t, Steps};
 
     pub fn steps() -> Steps<crate::APITestWorld> {
@@ -28,20 +37,22 @@ mod client_steps {
             )
             .when_regex_async(
                 "I GET (.*)",
-                t!(|world, matches, _step| {
-                    println!("{:?}", matches);
+                t!(|mut world, matches, _step| {
+                    world.response = Arc::new(AssertUnwindSafe(Some(reqwest::get(reqwest::Url::parse(&("http://localhost:24816/".to_string() + &matches[0].strip_prefix("I GET ").unwrap())).unwrap()).await.unwrap())));
                     world
                 }),
             )
-            .given(
-                "I am trying out Cucumber",
-                |mut world: crate::APITestWorld, _step| world,
-            )
-            .when("I consider what I am doing", |mut world, _step| world)
-            .then("I am interested in ATDD", |world, _step| world)
             .then_regex(
-                r"^we can (.*) rules with regex$",
-                |world, matches, _step| world,
+                "I should see a redirect to (.*)",
+                |world, matches, _step| {
+                    let arc = world.response.clone();
+                    let option = &arc.0;
+                    if let Some(response) = option {
+                        let string = matches[0].strip_prefix("I should see a redirect to ").unwrap().to_string();
+                        assert!(&response.url().as_str().starts_with(&string));
+                    }
+                    world
+                }
             );
 
         builder
--- tests/features/auth_redirect.feature
+++ tests/features/auth_redirect.feature
@@ -1,5 +1,5 @@
 Feature: Get a link the correct OAuth page.
-Scenario: GitHub OAuth start.
+Scenario: GitHub OAuth start v1.
 Given I have an instance of wirc on localhost
-When I GET /api/v1/login/github
-Then I should see a redirect to https://github.com/login/oauth/authorize
+When I GET api/v1/login/github
+Then I should see a redirect to https://github.com/login
