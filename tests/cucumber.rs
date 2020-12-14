fe8029bf64a5565d466a47b1294d2b8469210f36
willemml <willem@leit.so>
2020-12-13 22:59:44 -0800

    use cucumber macros

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,70 +1,49 @@
 use async_trait::async_trait;
+use cucumber_rust::{given, then, when, World, WorldInit};
 use reqwest::Response;
 use std::{convert::Infallible, panic::AssertUnwindSafe};
-use std::sync::Arc;
 
-#[derive(Clone)]
-pub struct APITestWorld {
-    response: Arc<AssertUnwindSafe<Option<Response>>>
+#[derive(WorldInit)]
+pub struct MyWorld {
+    wirc_running: bool,
+    response: Option<AssertUnwindSafe<Response>>,
 }
 
 #[async_trait(?Send)]
-impl cucumber::World for APITestWorld {
+impl World for MyWorld {
     type Error = Infallible;
 
     async fn new() -> Result<Self, Infallible> {
         Ok(Self {
-            response: Arc::new(AssertUnwindSafe(None))
+            wirc_running: false,
+            response: None,
         })
     }
 }
 
-mod client_steps {
-    use std::{panic::AssertUnwindSafe, sync::Arc};
+#[given("I have an instance of wirc on localhost")]
+async fn wirc_running(world: &mut MyWorld) {
+    tokio::task::spawn(wirc_server::run());
+    world.wirc_running = true;
+}
 
-    use cucumber::{t, Steps};
+#[when(regex = r"I GET (.*)")]
+async fn get_url(world: &mut MyWorld, url: String) {
+    assert!(world.wirc_running);
+    world.response = Some(AssertUnwindSafe(
+        reqwest::get(reqwest::Url::parse(&url).unwrap())
+            .await
+            .unwrap(),
+    ));
+}
 
-    pub fn steps() -> Steps<crate::APITestWorld> {
-        let mut builder: Steps<crate::APITestWorld> = Steps::new();
-
-        builder
-            .given_async(
-                "I have an instance of wirc on localhost",
-                t!(|mut world, _step| {
-                    tokio::task::spawn(wirc_server::run());
-                    world
-                }),
-            )
-            .when_regex_async(
-                "I GET (.*)",
-                t!(|mut world, matches, _step| {
-                    world.response = Arc::new(AssertUnwindSafe(Some(reqwest::get(reqwest::Url::parse(&("http://localhost:24816/".to_string() + &matches[0].strip_prefix("I GET ").unwrap())).unwrap()).await.unwrap())));
-                    world
-                }),
-            )
-            .then_regex(
-                "I should see a redirect to (.*)",
-                |world, matches, _step| {
-                    let arc = world.response.clone();
-                    let option = &arc.0;
-                    if let Some(response) = option {
-                        let string = matches[0].strip_prefix("I should see a redirect to ").unwrap().to_string();
-                        assert!(&response.url().as_str().starts_with(&string));
-                    }
-                    world
-                }
-            );
-
-        builder
-    }
+#[then(regex = r"I should be redirected to (.*)")]
+fn redirect_to(world: &mut MyWorld, url: String) {
+    assert!(&world.response.as_deref().unwrap().url().as_str().starts_with(&url));
 }
 
 #[tokio::main]
 async fn main() {
-    // Do any setup you need to do before running the Cucumber runner.
-    // e.g. setup_some_db_thing()?;
-    let runner = cucumber::Cucumber::<APITestWorld>::new()
-        .features(&["./tests/features/"])
-        .steps(client_steps::steps());
-    runner.run().await;
+    let runner = MyWorld::init(&["tests/features"]);
+    runner.run_and_exit().await;
 }
--- tests/features/auth_redirect.feature
+++ tests/features/auth_redirect.feature
@@ -1,5 +1,5 @@
 Feature: Get a link the correct OAuth page.
 Scenario: GitHub OAuth start v1.
 Given I have an instance of wirc on localhost
-When I GET api/v1/login/github
-Then I should see a redirect to https://github.com/login
+When I GET http://localhost:24816/api/v1/login/github
+Then I should be redirected to https://github.com/login
