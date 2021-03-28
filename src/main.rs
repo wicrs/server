70bb8cb3be5ca26f9a25d30444b5b099c2735952
willemml <willem@leit.so>
2021-03-28 14:54:27 -0700

    use exit instead of expect to avoid extra output for config load

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use std::process::exit;
+
 use wicrs_server::{config::Config, httpapi::server};
 
 fn load_config(path: &str) -> Config {
-    let read = std::fs::read_to_string(path).expect("Failed to load config.json.");
-    serde_json::from_str::<Config>(&read)
-        .expect("config.json does not contain a valid configuration.")
+    if let Ok(read) = std::fs::read_to_string(path) {
+        if let Ok(config) = serde_json::from_str::<Config>(&read) {
+            return config;
+        } else {
+            println!("config.json does not contain a valid configuration.");
+            exit(1);
+        }
+    } else {
+        println!("Failed to load config.json.");
+        exit(1);
+    }
 }
 
 #[actix_web::main]
