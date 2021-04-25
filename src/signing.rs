ebb2ef7dc159b460e5f39603ae50b13a829b7453
willemml <willem@leit.so>
2021-04-24 17:33:37 -0700

    functions for signing (both signatures) and extracting/verifying signed and double signed messages

--- src/signing.rs
+++ src/signing.rs
@@ -16,8 +16,8 @@ use pgp::{
 };
 use smallvec::*;
 
-pub const SECRET_KEY_PATH: &str = "data/secret_key_rpgp";
-pub const PUBLIC_KEY_PATH: &str = "data/secret_key_rpgp.pub";
+pub const SECRET_KEY_PATH: &str = "data/secret_key.asc";
+pub const PUBLIC_KEY_PATH: &str = "data/public_key.asc";
 
 pub struct KeyPair {
     pub secret_key: SignedSecretKey,
@@ -27,8 +27,8 @@ pub struct KeyPair {
 impl KeyPair {
     pub fn new<S: Into<String>>(id: S) -> Result<Self> {
         let secret_key = SecretKeyParamsBuilder::default()
-            .key_type(KeyType::Rsa(2048))
-            .can_create_certificates(false)
+            .key_type(KeyType::Rsa(4096))
+            .can_create_certificates(true)
             .can_sign(true)
             .primary_user_id(id.into())
             .preferred_symmetric_algorithms(smallvec![SymmetricKeyAlgorithm::AES256,])
@@ -61,6 +61,17 @@ impl KeyPair {
             public_key,
         })
     }
+
+    pub fn load_or_create<S: Into<String>>(id: S) -> Result<Self> {
+        let result = if let Ok(key_pair) = KeyPair::load() {
+            key_pair
+        } else {
+            let key_pair = KeyPair::new(id)?;
+            key_pair.save()?;
+            key_pair
+        };
+        Ok(result)
+    }
 }
 
 impl Message {
@@ -71,16 +82,51 @@ impl Message {
     ) -> Result<OpenPGPMessage> {
         Ok(OpenPGPMessage::try_from(self)?.sign(&secret_key, password, HashAlgorithm::SHA2_256)?)
     }
+
+    pub fn from_double_signed(message_str: &str) -> Result<Self> {
+        let client_signed = OpenPGPMessage::from_string(message_str)?.0;
+        if let Some(d) = client_signed.get_literal() {
+            if let Some(s) = d.to_string() {
+                return Message::try_from(OpenPGPMessage::from_string(&s)?.0);
+            }
+        }
+        Err(Error::InvalidMessage)
+    }
+
+    pub fn from_double_signed_verify(
+        message_str: &str,
+        server_public_key: &impl PublicKeyTrait,
+        client_public_key: &impl PublicKeyTrait,
+    ) -> Result<Message> {
+        let client_signed = OpenPGPMessage::from_string(message_str)?.0;
+        client_signed.verify(client_public_key)?;
+        if let Some(d) = client_signed.get_literal() {
+            if let Some(s) = d.to_string() {
+                let server_signed = OpenPGPMessage::from_string(&s)?.0;
+                server_signed.verify(server_public_key)?;
+                return Message::try_from(server_signed);
+            }
+        }
+        Err(Error::InvalidMessage)
+    }
 }
 
 impl TryFrom<&Message> for OpenPGPMessage {
-    type Error = serde_json::Error;
+    type Error = Error;
 
     fn try_from(m: &Message) -> Result<Self, Self::Error> {
         Ok(OpenPGPMessage::Literal(LiteralData::from_str(
             &m.id.to_string(),
             &serde_json::to_string(&m)?,
         )))
+    }
+}
+
+impl TryFrom<Message> for OpenPGPMessage {
+    type Error = Error;
+
+    fn try_from(value: Message) -> Result<Self, Self::Error> {
+        Self::try_from(&value)
     }
 }
 
@@ -95,6 +141,14 @@ impl TryFrom<&OpenPGPMessage> for Message {
                 .to_string()
                 .map_or_else(|| Err(Error::InvalidMessage), Ok)?,
         )?)
+    }
+}
+
+impl TryFrom<OpenPGPMessage> for Message {
+    type Error = Error;
+
+    fn try_from(value: OpenPGPMessage) -> Result<Self, Self::Error> {
+        Self::try_from(&value)
     }
 }
 
@@ -120,14 +174,7 @@ pub fn sign_and_verify() -> Result {
     let KeyPair {
         secret_key,
         public_key,
-    } = if let Ok(key_pair) = KeyPair::load() {
-        key_pair
-    } else {
-        let key_pair =
-            KeyPair::new("WICRS Server <server@wic.rs>").expect("Failed to create a new key pair.");
-        let _ = key_pair.save();
-        key_pair
-    };
+    } = KeyPair::load_or_create("WICRS Server <server@wic.rs>")?;
 
     println!("key_id: {}\n", hex::encode(secret_key.key_id().to_vec()));
 
@@ -137,13 +184,18 @@ pub fn sign_and_verify() -> Result {
 
     let msg_signed = message.sign(&secret_key, passwd_fn)?;
     let msg_armored_str = msg_signed.to_armored_string(None)?;
-    println!("{}", msg_armored_str);
+
+    println!("{}\n", msg_armored_str);
+
+    let final_message = sign_final(&msg_armored_str, &public_key, &secret_key, passwd_fn)?;
+    let final_message_str = final_message.to_armored_string(None)?;
+
+    println!("{}\n", final_message_str);
 
     let _ = println!(
         "{}",
-        Message::try_from(&OpenPGPMessage::from_string(&msg_armored_str)?.0)?
+        Message::from_double_signed_verify(&final_message_str, &public_key, &public_key)?
     );
 
-    let _ = sign_final(&msg_armored_str, &public_key, &secret_key, passwd_fn)?;
     Ok(())
 }
